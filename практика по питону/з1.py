4bec4b949a285497341f9bd870c27504450c3fac
NOE <94932936+ragamunf@users.noreply.github.com>
2024-09-21 15:31:02 +0300

    Add files via upload

---
+++ практика по питону/з1.py
@@ -0,0 +1,6 @@
+name = input('Ваши фамилия, имя? ')
+age = input('Сколько вам лет? ')
+place = input('Где вы живете? ')
+print("Ваши фамилия, имя", name)
+print("Ваш возраст", age)
+print("Вы живёте в", place)
