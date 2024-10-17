4b8567ab23ff015a0039184e901b69bbfccd1ef8
NOE <94932936+ragamunf@users.noreply.github.com>
2024-10-17 07:11:41 +0300

    Add files via upload

---
+++ практика по питону/з2. №1.py
@@ -0,0 +1,8 @@
+# -- coding: utf-8 --
+
+from math import *
+x, y, z = 14.26, -1.22, 0.035
+u = 2*cos(x - 2/3)*(1 + (pow(z, 2)/(3 - pow(z, 2)/5)))
+d = 1/2 + pow(sin(y), 2)
+s = u/d
+print('s = {0:.6}'.format(s))
