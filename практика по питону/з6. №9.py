215a42135c9f8f526e3f5d38d7a3a58bfcd9894f
NOE <94932936+ragamunf@users.noreply.github.com>
2024-10-19 12:42:34 +0300

    Update з6. №9.py

--- практика по питону/з6. №9.py
+++ практика по питону/з6. №9.py
@@ -1,10 +1,9 @@
 # -- coding: utf-8 --
 
 s = input('Введите заданное слово: ').lower()
-f = input('Введите текст: ').split()
+f = input('Введите текст: ').lower().split()
 for i_word in range(len(f)):
     for symbol in f[i_word]:
         if not symbol.isalpha():
            f[i_word] = f[i_word].replace(symbol, '') 
-    f[i_word] = f[i_word].lower()
 print(f.count(s))
