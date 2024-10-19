21c23e9bb09730833566c0772ea54e19c26b6d85
NOE <94932936+ragamunf@users.noreply.github.com>
2024-10-19 16:27:34 +0300

    Update №2.py

--- практика по питону/з4/№2.py
+++ практика по питону/з4/№2.py
@@ -3,8 +3,8 @@
 '''Даны два целых числа A и В. Выведите все числа от A до B включительно, в порядке возрастания,
 если A < B, или в порядке убывания в противном случае.'''
 
-a, b = int(input()), int(input())
 def nums():
+    a, b = int(input()), int(input())
     if a < b:
         for x in range(a, b+1): print(x)
     else:
