15cf51d3e8b7e8f350c94fd034a1f910aa41bcfd
NOE <94932936+ragamunf@users.noreply.github.com>
2024-10-19 16:27:04 +0300

    Update №3.py

--- практика по питону/з4/№3.py
+++ практика по питону/з4/№3.py
@@ -2,10 +2,10 @@
 
 '''Даны два целых числа A и В, A>B. Выведите все нечётные числа от A до B включительно, в порядке убывания'''
 
-a, b = int(input()), int(input())
 def ne_nums():
+    a, b = int(input()), int(input())
     if a > b:
         for x in range(a, b-1, -1):
-            if x%2!=0: print(x)
+            if x % 2 != 0: print(x)
 ne_nums()
 
