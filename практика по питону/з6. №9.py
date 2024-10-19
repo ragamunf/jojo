571d75b3928a1181130f1fdd67489577ce67d29d
NOE <94932936+ragamunf@users.noreply.github.com>
2024-10-19 22:07:33 +0300

    Update and rename практика по питону/з6. №9.py to практика по питону/з6/№9.py

--- практика по питону/з6. №9.py
+++
@@ -1,9 +0,0 @@
-# -- coding: utf-8 --
-
-s = input('Введите заданное слово: ').lower()
-f = input('Введите текст: ').lower().split()
-for i_word in range(len(f)):
-    for symbol in f[i_word]:
-        if not symbol.isalpha():
-           f[i_word] = f[i_word].replace(symbol, '') 
-print(f.count(s))
