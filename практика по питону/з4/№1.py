199617ef34d8babb0d52ff2d1e8c683c1f60f073
NOE <94932936+ragamunf@users.noreply.github.com>
2024-10-19 16:27:56 +0300

    Update №1.py

--- практика по питону/з4/№1.py
+++ практика по питону/з4/№1.py
@@ -2,8 +2,9 @@
 
 '''Даны два целых числа A и B (при этом A ≤ B). Выведите все числа от A до B включительно.'''
 
-a, b = int(input()), int(input())
+
 def nums():
+    a, b = int(input()), int(input())
     for x in range(a , b+1): print(x)
 nums()
 
