b3eec3636ffb3c5aad12a1d607454864db63b630
NOE <94932936+ragamunf@users.noreply.github.com>
2024-10-20 16:07:26 +0300

    Update №1.py

--- практика по питону/з2/№1.py
+++ практика по питону/з2/№1.py
@@ -5,4 +5,4 @@ x, y, z = 14.26, -1.22, 0.035
 u = 2*cos(x - 2/3)*(1 + (pow(z, 2)/(3 - pow(z, 2)/5)))
 d = 1/2 + pow(sin(y), 2)
 s = u/d
-print('s = {0:.6}'.format(s))
+print('s = {0:.6f}'.format(s))
