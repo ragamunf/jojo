ddd98b4ea3d29827fb55f54063eb716dafb2974a
NOE <94932936+ragamunf@users.noreply.github.com>
2024-10-20 16:09:37 +0300

    Add files via upload

---
+++ практика по питону/з2/з2 №3.py
@@ -0,0 +1,11 @@
+from math import *
+
+x = 3.74*10**(-2)
+y = -0.825
+z = 0.16*10**2
+
+ch = (1 + (sin(x+y))**2)*x**abs(y)
+zn = abs(x - 2*y/(1+x**2*y**2))
+s = ch/zn + (cos(atan(1/z)))**2
+print('s = {0:.5f}'.format(s))
+
