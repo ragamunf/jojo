ce0767a853140149c14a9681996b6fed42e663f1
NOE <94932936+ragamunf@users.noreply.github.com>
2024-10-20 12:05:34 +0300

    Update №5.py

--- практика по питону/з3/№5.py
+++ практика по питону/з3/№5.py
@@ -4,7 +4,6 @@
 
 def nums(a, b, c):
     return min(a, b, c)
-a, b, c = int(input('первое число: ')), int(input('второе число: ')),\
-          int(input('третье число: '))
+a, b, c = int(input('первое число: ')), int(input('второе число: ')), int(input('третье число: '))
 print(nums(a, b, c))
     
