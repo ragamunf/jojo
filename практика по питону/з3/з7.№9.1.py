df90e6f6f3e0bdcf69ec541db52a06299d65d149
NOE <94932936+ragamunf@users.noreply.github.com>
2024-11-01 19:28:04 +0300

    Add files via upload

---
+++ практика по питону/з3/з7.№9.1.py
@@ -0,0 +1,8 @@
+def l():
+    n = int(input('Введите длину массива: '))
+    s = [float(input(f'Введите элемент {x}: ')) for x in range(n)]
+    s_1 = [abs(a) for a in s]
+    return min(s_1), s[::-1]
+mn, s = l()
+print(f'Минимальный элемент: {mn}')
+print(s)
