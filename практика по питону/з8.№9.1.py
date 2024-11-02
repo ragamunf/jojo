d825e2458e803b55d38788a9a95c0882236c48a1
NOE <94932936+ragamunf@users.noreply.github.com>
2024-11-02 08:29:12 +0300

    Add files via upload

---
+++ практика по питону/з8.№9.1.py
@@ -0,0 +1,12 @@
+def f(n):
+    global count
+    if n!=0:
+        n -= sum([int(x) for x in str(n)])
+        count.append(n)
+        f(n)
+    return len(count)
+       
+n = int(input('введите число: '))
+count = []
+print(f(n))
+        
