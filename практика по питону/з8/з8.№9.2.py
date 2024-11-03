41520ddc50ad447b6455cd0dbb360e3c9122c4c2
NOE <94932936+ragamunf@users.noreply.github.com>
2024-11-03 08:33:48 +0300

    Add files via upload

---
+++ практика по питону/з8/з8.№9.2.py
@@ -0,0 +1,14 @@
+def f(x):
+    total = 1
+    for i in x: total*= i
+    print(f'для {x}\n произведение элементов равно: {total}, ср. ариф. равно: {sum(x)/len(x)}')
+
+from random import *
+
+a = [randint(12, 18) for x in range(4)]
+b = [randint(0, 10) for x in range(6)]
+c = [randint(19, 26) for x in range(5)]
+
+f(a), f(b), f(c)
+
+
