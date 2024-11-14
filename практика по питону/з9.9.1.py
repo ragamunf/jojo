fdb9a82799de4d7d07926406a271d12214bc3027
NOE <94932936+ragamunf@users.noreply.github.com>
2024-11-14 08:18:30 +0300

    Add files via upload

---
+++ практика по питону/з9.9.1.py
@@ -0,0 +1,26 @@
+# -- coding: utf-8 --
+
+def matrix():
+    k = int(input(f'Введите число k: '))
+    n = int(input('Введите число строк: '))
+    a = []
+    for i in range(n):
+        b = []
+        for j in range(n):
+            b.append(int(input(f'Введите [{i}, {j}] элемент: ')))
+        a.append(b)
+    d = []
+    for row in a:
+        for elem in row:
+            if elem % k == 0: d.append(elem)
+    return d
+
+res = matrix()
+
+print(f'Количество искомых элементов: {len(res)}')
+print(f'Максимальный из них: {max(res)}')
+            
+
+                 
+                 
+                 
