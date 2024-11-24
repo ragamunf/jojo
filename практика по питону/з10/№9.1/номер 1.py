828ce4436cc153f1536444f6b625de5654459bb4
mdina <94932936+ragamunf@users.noreply.github.com>
2024-11-24 14:42:24 +0300

    Update номер 1.py

--- практика по питону/з10/№9.1/номер 1.py
+++ практика по питону/з10/№9.1/номер 1.py
@@ -16,5 +16,5 @@ with open('movsarovamedinaramzanovna_u242_vvod.txt', 'r') as vvod, \
      open('movsarovamedinaramzanovna_u242_vivod.txt', 'w') as vivod:
     mx, res = matrix(vvod)
     for row in mx:
-        vivod.write(' '.join(list(map(str, row)))+'\n')
+        vivod.write(''.join(list(map(str, row)))+'\n')
     vivod.write(f'Количество искомых элементов: {len(res)}, максимальный из них: {max(res)}')
