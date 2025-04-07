63bb159b75e0a0c4da9fa88092026cdae0391263
medina <94932936+ragamunf@users.noreply.github.com>
2025-04-07 05:44:55 +0300

    Add files via upload

---
+++ пр2/SecondTask.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SecondTask {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите имя: ");
+        String name = in.nextLine();
+        var rightName = name.toUpperCase().charAt(0) + name.substring(1);
+        System.out.printf("Привет, %s!", rightName);
+        in.close();
+
+    }
+}
