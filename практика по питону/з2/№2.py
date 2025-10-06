0a8581dc147485b4d89ea556405e202b13dc9235
medina <marinagromgrom@gmail.com>
2025-10-06 09:55:50 +0300

    Remove 'практика по питону'

--- практика по питону/з2/№2.py
+++
@@ -1,9 +0,0 @@
-# -- coding: utf-8 --
-
-import math
-x, y, z = -4.5, 0.000075, -84.5
-u = math.pow(9+(x-y)**2, 1/3)
-d = x**2 + y**2 + 2
-r = math.pow(math.e, math.fabs(x-y))*math.tan(z)**3
-s = u/d - r
-print('s = {0:.5f}'.format(s))
--- практика по питону/з3/№4.py
+++
@@ -1,7 +0,0 @@
-# -- coding: utf-8 --
-
-def lace(a, b, l, N):
-    return 2*l + 2*b*(N-1) + a*(2*N-1)
-a, b, l, N = int(input()), int(input()), int(input()), int(input())
-print(lace(a, b, l, N))
-    
