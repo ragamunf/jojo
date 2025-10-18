6113a139936eed0cbc991955af6bb31d6083a491
medina <94932936+ragamunf@users.noreply.github.com>
2025-10-18 06:40:12 +0300

    Delete практика по питону directory

--- практика по питону/з1/№1.py
+++
@@ -1,8 +0,0 @@
-# -- coding: utf-8 --
-
-name = input('Ваши фамилия, имя? ')
-age = input('Сколько вам лет? ')
-place = input('Где вы живете? ')
-print("Ваши фамилия, имя", name)
-print("Ваш возраст", age)
-print("Вы живёте в", place)
--- практика по питону/з12/блок_А_№4.py
+++
@@ -1,9 +0,0 @@
-# -- coding: utf-8 --
-
-def f(n):
-
-    if n == 0: return 0
-    return n%10 + f(n//10)
-
-n = int(input('Введите число n: '))
-print(f'Сумма цифр числа {n} равна: {f(n)}')
